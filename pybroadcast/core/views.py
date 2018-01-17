d0b949b366cb8cd6726fbe0a914087c22d80fec7
ftbarata <fellipetheophilo@gmail.com>
2018-01-17 15:34:31 -0200

    Fixed delete user view usuarios_autorizados variable

--- pybroadcast/core/views.py
+++ pybroadcast/core/views.py
@@ -171,7 +171,8 @@ def configuracoes(request):
 
 def deleteAuthorizedUser(request, id):
     if request.user.is_authenticated:
-        usuarios_autorizados = UsuariosAutorizados.objects.all()
+        estado_lotacao = str(_get_ldap_user_attrs_as_dict_of_lists(username=request.user, attr_list=['st'])['st'][0]).upper()
+        usuarios_autorizados = UsuariosAutorizados.objects.all().filter(Q(estado_lotacao__iexact=estado_lotacao) | Q(adicionado_por__iexact=request.user)).order_by('-timestamp')
         remote_addr = request.META['REMOTE_ADDR']
         nome_completo = request.session['nome_completo']
         removido_por = request.user
